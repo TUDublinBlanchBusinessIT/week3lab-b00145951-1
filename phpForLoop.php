cee27a33fab3bd12b0bfe44dfe18225438d2b309
B00145951 Sophie Daly <B00145951@mytudublin.ie>
2023-10-04 10:03:43 +0100

    changed 10 to 20

--- phpForLoop.php
+++ phpForLoop.php
@@ -3,7 +3,7 @@
 #put your name and the date here
 #put a descripition of what the code does here - 
 #this program uses a loop to count up to 10 and puts the value of the loop counter out to the screen on each iteration
-for ($i=1;$i<=10;$i++) {
+for ($i=0;$i<=20;$i++) {
     echo "$i, ";
 }
 
